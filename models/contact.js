dd1152aa9b89c0cdfe639cc69894c724411b28ff
Ali Mokheamer <mokhy12345@hotmail.com>
2019-02-09 14:53:23 -0500

    added create functionality to the db

--- models/contact.js
+++ models/contact.js
@@ -14,4 +14,4 @@ let contactSchema = mongoose.Schema({
     collection: "first"
 });
 
-module.exports = mongoose.model('test', contactSchema);
+module.exports = mongoose.model('contact', contactSchema);
--- routes/contact.js
+++ routes/contact.js
@@ -3,7 +3,7 @@ let router = express.Router();
 let mongoose = require('mongoose');
 
 /* Create a reference to the db schema */
-let contact = require('../models/contact'); // reference to contact in models folder
+let contactModel = require('../models/contact'); // reference to contact in models folder
 
 /* GET Contact list page - READ Operation */
 router.get('/', (req, res, next) =>{
@@ -18,6 +18,38 @@ router.get('/', (req, res, next) =>{
                 title: 'Contact List',
                 contactList: contactList
             });
+        }
+    });
+});
+
+/* GET Route for the Add page
+  this will display the add Page
+*/
+ 
+router.get('/add', (req, res, next) => {
+    res.render('contacts/add'), {
+    title: 'Contact List'
+    }
+});
+
+/* Post route for processing the Add page  */
+router.post('/add', (req, res, next) => {
+
+    console.log(req.body);
+
+    let newContact = contactModel({
+        "firstName": req.body.firstName,
+        "lastName": req.body.lastName,
+        "age": req.body.age
+    });
+
+    contactModel.create(newContact, (err, contactModel) => {
+        if (err) {  // this error gives a server error because it cannot add the content
+            console.log(err);
+            res.end(err);
+        } else {
+            // refresh the contact list
+            res.redirect('/contact-list');
         }
     });
 });
